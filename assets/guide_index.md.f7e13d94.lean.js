cd68eeb7d3ab2746fcf9e7e738fffc4b51269215
krmax44 <krmax44@users.noreply.github.com>
2021-04-28 16:38:49 +0000

    deploy: 36b9907fe6802cb770d482072c592aed8032f049

---
+++ assets/guide_index.md.f7e13d94.lean.js
@@ -0,0 +1,1 @@
+import{o as n,c as s,b as a}from"./app.7cbc1c56.js";const t='{"title":"What is this plugin?","description":"","frontmatter":{"title":"What is this plugin?"},"relativePath":"guide/index.md","lastUpdated":1619627917412}',p={},e=a('',7);p.render=function(a,t,p,o,i,c){return n(),s("div",null,[e])};export default p;export{t as __pageData};
